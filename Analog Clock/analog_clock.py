12e8fb47490341e7e09eefa05354b16e40e50ad1
Debasish6 <iamdebasishghosh6@gmail.com>
2024-09-20 17:58:12 +0530

    Analog Clock Successfully Completed

--- Analog Clock/analog_clock.py
+++ Analog Clock/analog_clock.py
@@ -1,27 +1,41 @@
 import cv2
 import numpy as np
 from constants import CANVAS_SIZE, RADIUS , CENTER, COLOURS
-from helper_function import get_ticks
+from helper_function import get_ticks,get_hand
 
 canvas = np.zeros(CANVAS_SIZE,dtype = np.uint8)
 
 canvas[:] = [255,255,255]
 
-hour_init,hour_dest =get_ticks()
+hour_init,hour_dest,number_position =get_ticks()
 
 # Drawing a Circle
 cv2.circle(canvas,CENTER,RADIUS+20,(125,125,125),3)
  
 # Creating tic lines and circles
-
+k = 0
 for i in range(len(hour_init)):
     if i % 5 == 0:
         if i % 15 == 0:
+            print(f"number_position : {number_position[k]}")
             cv2.line(canvas,hour_init[i],hour_dest[i],(0,0,255),3)
+            cv2.putText(canvas,f'{(k + 1) * 3}',number_position[k],cv2.FONT_HERSHEY_TRIPLEX,1.5,(0,0,0),2,cv2.LINE_AA)
+            k = k + 1
         else:
             cv2.line(canvas,hour_init[i],hour_dest[i],(0,0,0),3)
     else:
         cv2.circle(canvas,hour_init[i],5,(0,0,0),-1)
 
-cv2.imshow('Clock',canvas)
-cv2.waitKey(0)
+while True:
+    image_copy = canvas.copy()
+    
+    new_image = get_hand(image_copy)
+
+    cv2.circle(image_copy,CENTER,8,(0,0,0),10)
+    cv2.circle(image_copy,CENTER,7,(125,125,125),-1)
+
+    cv2.imshow('Clock',image_copy)
+    if cv2.waitKey(3) == ord('q'):
+        break
+    
+cv2.destroyAllWindows()
--- Analog Clock/helper_function.py
+++ Analog Clock/helper_function.py
@@ -6,6 +6,7 @@ from constants import CANVAS_SIZE, RADIUS , CENTER, COLOURS
 def get_ticks():
     hours_init = []
     hours_dest = []
+    number_position =[]
     
     for i in range(0,360,6):
         x_coordinate = int(CENTER[0] + RADIUS * math.cos( i * math.pi / 180))
@@ -17,12 +18,38 @@ def get_ticks():
         y_coordinate = int(CENTER[1] + (RADIUS-20) * math.sin( i * math.pi / 180))
         hours_dest.append((x_coordinate,y_coordinate))
     
-    return hours_init,hours_dest
+    for i in range(0,360,90):
+        x_coordinate = int(CENTER[0] - 15 + (RADIUS-50) * math.cos( i * math.pi / 180))
+        y_coordinate = int(CENTER[1] + 15 + (RADIUS-50) * math.sin( i * math.pi / 180))
+        number_position.append((x_coordinate,y_coordinate))
+    
+    return hours_init,hours_dest,number_position
 
 def get_hand(image):
-    timenow = datetime.now.time()
-    hour = timenow.hour
+    timenow = datetime.now()
+    hour = math.fmod(timenow.hour, 12)
     minute = timenow.minute
     second = timenow.second
+    
+    second_angle = math.fmod(second * 6 + 270 , 360)
+    minute_angle = math.fmod(minute * 6 + 270 , 360)
+    hour_angle = math.fmod((hour * 30) + (minute / 2) + 270 , 360)
+    
+    # Drawing Second hand
+    x_second = int(CENTER[0] + (RADIUS-50) * math.cos( second_angle * math.pi / 180))
+    y_second = int(CENTER[1] + (RADIUS-50) * math.sin( second_angle * math.pi / 180))
+    cv2.line(image,CENTER,(x_second,y_second),(0,0,0),2)
+    
+    # Drawing Minute hand
+    x_minute = int(CENTER[0] + (RADIUS-30) * math.cos( minute_angle * math.pi / 180))
+    y_minute = int(CENTER[1] + (RADIUS-30) * math.sin( minute_angle * math.pi / 180))
+    cv2.line(image,CENTER,(x_minute,y_minute),(0,0,0),5)
+    
+    # Drawing Hour hand
+    x_hour = int(CENTER[0] + (RADIUS-100) * math.cos( hour_angle * math.pi / 180))
+    y_hour = int(CENTER[1] + (RADIUS-100) * math.sin( hour_angle * math.pi / 180))
+    cv2.line(image,CENTER,(x_hour,y_hour),(0,0,0),8)
+    
+    return image
     
     
